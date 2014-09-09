71f59177855cb4719228ff2434bc772b0639e5f3
abetusk <abetusk@gmail.com>
2014-09-09 00:13:43 -0400

    further library import development

--- scripts/libmodimport.py
+++ scripts/libmodimport.py
@@ -11,10 +11,16 @@ import magic
 import os.path
 import shutil
 
-TMP_DIR = "/tmp/" + str(uuid.uuid4())
+g_verbose_flag = False
 
-# DEBUG
-print TMP_DIR
+UUID = str(uuid.uuid4())
+TMP_DIR = "/tmp/" + UUID
+
+LIB_LOC_JSON = {}
+MOD_LOC_JSON = {}
+
+LIB_ELE_LIST_JSON = {}
+MOD_ELE_LIST_JSON = {}
 
 sp.call(" mkdir -p " + TMP_DIR, shell=True  )
 
@@ -57,6 +63,7 @@ def kicad_magic( fn ):
 parser = argparse.ArgumentParser(description='Description of your program')
 parser.add_argument('-i','--input', help='Input file', required=True)
 parser.add_argument('-o','--output-dir', help='Output directory', required=True)
+parser.add_argument('-V','--verbose', help='Verbose', action='store_true' )
 #parser.add_argument('-m','--mime', help='Output in MIME type', action='store_true' )
 #parser.add_argument('-u','--user-id', help='User ID' )
 #parser.add_argument('-p','--project-id', help='Project ID' )
@@ -64,9 +71,14 @@ args = vars(parser.parse_args())
 
 inp_fn = args["input"]
 out_dir = args["output_dir"]
+g_verbose_flag = args["verbose"]
 #userId = args["user_id"]
 #projectId = args["project_id"]
 #showmime = args["mime"]
+
+# DEBUG
+if g_verbose_flag:
+  print "#TMP_DIR:", TMP_DIR
 
 if out_dir is None:
   out_dir = "./"
@@ -91,12 +103,56 @@ def process_modlib( root, fn ):
   if ktype == "lib":
     lib_dir = os.path.join( TMP_DIR, "eeschema", "json", fin_rel_dir )
     sp.call(" mkdir -p " + lib_dir , shell=True  )
-    sp.call( [ libjson_exec, os.path.join(root, fn) , lib_dir + "/" ] )
+    try:
+      liblist_str = sp.check_output( [ libjson_exec, os.path.join(root, fn) , lib_dir + "/" ] )
+
+      liblist = liblist_str.split("\n")
+      for name_loc in liblist:
+        v = name_loc.split(' ')
+        if len(v) < 2: continue
+        name,loc = v[0], v[1][len(TMP_DIR)+1:]
+        LIB_LOC_JSON[ name ] = { "name" : name, "location" : loc }
+
+        list_ele = { "id" : name, "name" : name, "data" : loc, "type" : "element" }
+        ele = { "id" : fin_rel_dir, "name" : fin_rel_dir }
+        ele["data"] = os.path.join( "eeschema", "json", fin_rel_dir )
+        ele["type"] = "list"
+        ele["list"] = []
+        if fin_rel_dir in LIB_ELE_LIST_JSON:
+          ele = LIB_ELE_LIST_JSON[fin_rel_dir]
+        ele["list"].append( list_ele )
+        LIB_ELE_LIST_JSON[fin_rel_dir] = ele
+
+    except Exception,e:
+      print "modjson failed:", str(e)
+
 
   if ktype == "mod":
     mod_dir = os.path.join( TMP_DIR, "pcb", "json", fin_rel_dir )
     sp.call(" mkdir -p " + mod_dir  , shell=True  )
-    k = sp.call( [ modjson_exec, os.path.join(root, fn) , mod_dir + "/" ] )
+
+    try:
+      modlist_str = sp.check_output( [ modjson_exec, os.path.join(root, fn) , mod_dir + "/" ] )
+
+      modlist = modlist_str.split("\n")
+      for name_loc in modlist:
+        v = name_loc.split(' ')
+        if len(v) < 2: continue
+        name,loc = v[0], v[1][len(TMP_DIR)+1:]
+        MOD_LOC_JSON[ name ] = { "name" : name, "location" : loc }
+
+        list_ele = { "id" : name, "name" : name, "data" : loc, "type" : "element" }
+        ele = { "id" : fin_rel_dir, "name" : fin_rel_dir }
+        ele["data"] = os.path.join( "pcb", "json", fin_rel_dir )
+        ele["type"] = "list"
+        ele["list"] = []
+        if fin_rel_dir in MOD_ELE_LIST_JSON:
+          ele = MOD_ELE_LIST_JSON[fin_rel_dir]
+        ele["list"].append( list_ele )
+        MOD_ELE_LIST_JSON[fin_rel_dir] = ele
+
+    except Exception,e:
+      print "modjson failed:", str(e)
 
 
 def process_dir( base_dir ):
@@ -133,11 +189,11 @@ def process_file( inp_fn, recur=0, processed={} ):
   typ = magic.from_file( inp_fn, mime=True )
   app,mtype = typ.split("/")
 
-  #DEBUG
-  print typ, app, mtype
+  if g_verbose_flag:
+    print "#", typ, app, mtype
 
   if mtype == "gzip":
-    print "gzip"
+    if g_verbose_flag: print "# gzip"
 
     bn = os.path.splitext( os.path.basename( inp_fn ) )[0]
     cmd = "gunzip -c " + inp_fn + " > " + TMP_DIR  + "/" + bn
@@ -145,14 +201,14 @@ def process_file( inp_fn, recur=0, processed={} ):
     process_file( TMP_DIR + "/" + bn, recur+1 )
 
   elif mtype == "zip":
-    print "zip"
+    if g_verbose_flag: print "# zip"
 
     cmd = [ "unzip", "-q", inp_fn , "-d", TMP_DIR ]
     x = sp.call( cmd )
     process_dir( TMP_DIR )
 
   elif mtype == "x-bzip2":
-    print "bzip2"
+    if g_verbose_flag: print "# bzip2"
 
     bn = os.path.splitext( os.path.basename( inp_fn ) )[0]
     cmd = "bunzip2 -c " + inp_fn + " > " + TMP_DIR  + "/" + bn
@@ -160,7 +216,7 @@ def process_file( inp_fn, recur=0, processed={} ):
     process_file( TMP_DIR + "/" + bn, recur+1 )
 
   elif mtype == "x-tar":
-    print "x-tar"
+    if g_verbose_flag: print "# x-tar"
 
     bn = os.path.splitext( os.path.basename( inp_fn ) )[0]
     cmd = "tar xC " + TMP_DIR + " -f " + inp_fn 
@@ -170,7 +226,7 @@ def process_file( inp_fn, recur=0, processed={} ):
 
 
   elif mtype == "directory":
-    print "directory"
+    if g_verbose_flag: print "# directory"
 
     cmd = [ "mv", inp_fn, TMP_DIR ]
     sp.call( [ "mv", inp_fn, TMP_DIR ] )
@@ -178,18 +234,34 @@ def process_file( inp_fn, recur=0, processed={} ):
     process_dir( TMP_DIR )
 
   elif app == "text":
+    if g_verbose_flag: print "# text"
+
     process_modlib( TMP_DIR, inp_fn )
 
 
 
-#if not os.path.isfile( inp_fn ):
-#  sys.stderr.write( inp_fn + "\n" )
-#  parser.print_help()
-#  sys.exit(1)
-
-
-
 process_file( inp_fn )
-
 shutil.rmtree( TMP_DIR )
+
+sp.call(" mkdir -p " + out_dir  + "/json" , shell=True  )
+
+if len(LIB_LOC_JSON) > 0:
+  floc = open( os.path.join( out_dir, "json", UUID + "_component_location.json" ), "w" )
+  floc.write( json.dumps( LIB_LOC_JSON, indent=2 ) )
+  floc.close()
+
+if len(LIB_ELE_LIST_JSON) > 0:
+  flist = open( os.path.join( out_dir, "json", UUID + "_component_list.json" ), "w" )
+  flist.write( json.dumps( LIB_ELE_LIST_JSON, indent=2 ) )
+  flist.close()
+
+if len(MOD_LOC_JSON) > 0:
+  floc = open( os.path.join( out_dir, "json", UUID + "_footprint_location.json" ), "w" )
+  floc.write( json.dumps( MOD_LOC_JSON, indent=2 ) )
+  floc.close()
+
+if len(MOD_ELE_LIST_JSON) > 0:
+  flist = open( os.path.join( out_dir, "json", UUID + "_footprint_list.json" ), "w" )
+  flist.write( json.dumps( MOD_ELE_LIST_JSON, indent=2 ) )
+  flist.close()
 
---
+++ scripts/libmodloclist.py
@@ -0,0 +1,110 @@
+#!/usr/bin/python
+#
+# Generate the location and list JSON files
+# for use by the schematic and board editor.
+#
+
+import json
+import urllib
+import re
+import os
+import os.path
+import sys
+import subprocess as sp
+import meowaux as mew
+
+def get_name(x):
+  return x["name"].lower()
+
+
+init_base = "/var/www/json"
+
+init_comp_loc_fn = init_base + "/component_location.json"
+init_comp_lst_fn = init_base + "/component_list_default.json"
+init_foot_loc_fn = init_base + "/footprint_location.json"
+init_foot_lst_fn = init_base + "/footprint_list_default.json"
+
+comp_loc = json.loads( mew.json_slurp_file( init_comp_loc_fn ) )
+comp_lst = json.loads( mew.json_slurp_file( init_comp_lst_fn ) )
+foot_loc = json.loads( mew.json_slurp_file( init_foot_loc_fn ) )
+foot_lst = json.loads( mew.json_slurp_file( init_foot_lst_fn ) )
+
+if len(sys.argv) < 2:
+  print "provide input base directory"
+  sys.exit(1)
+
+base = sys.argv[1]
+
+#for fn in os.listdir( base + "/json" ):
+for fn in os.listdir( base ):
+  if fn.endswith("_component_location.json"):
+    #f = open(base + "/json/" + fn)
+    f = open(base + fn)
+    x = json.load(f)
+    f.close()
+
+    for k in x:
+      comp_loc[k] = x[k]
+
+  if fn.endswith("_component_list.json"):
+
+    #f = open( base + "/json/" + fn)
+    f = open( base + fn)
+    x = json.load(f);
+    f.close()
+
+    for k in x:
+      comp_lst.append( x[k] )
+
+  if fn.endswith("_footprint_location.json"):
+
+    #f = open(base + "/json/" + fn)
+    f = open(base + fn)
+    x = json.load(f)
+    f.close()
+
+    for k in x:
+      foot_loc[k] = x[k]
+
+
+  if fn.endswith("_footprint_list.json"):
+
+    #f = open( base + "/json/" + fn)
+    f = open( base + fn)
+    x = json.load(f);
+    f.close()
+
+    for k in x:
+      foot_lst.append( x[k] )
+
+
+for i,v in enumerate(comp_lst):
+  comp_lst[i]["list"] = sorted( comp_lst[i]["list"], key=get_name )
+
+for i,v in enumerate(foot_lst):
+  foot_lst[i]["list"] = sorted( foot_lst[i]["list"], key=get_name )
+
+comp_lst = sorted( comp_lst, key=get_name )
+foot_lst = sorted( foot_lst, key=get_name )
+
+#f = open( base + "/json/component_location.json", "w" )
+f = open( base + "/component_location.json", "w" )
+f.write( json.dumps( comp_loc, indent=2 ) )
+f.close()
+
+#f = open( base + "/json/component_list_default.json", "w" )
+f = open( base + "/component_list_default.json", "w" )
+f.write( json.dumps( comp_lst, indent=2 ) )
+f.close()
+
+#f = open( base + "/json/footprint_location.json", "w" )
+f = open( base + "/footprint_location.json", "w" )
+f.write( json.dumps( foot_loc, indent=2 ) )
+f.close()
+
+#f = open( base + "/json/footprint_list_default.json", "w" )
+f = open( base + "/footprint_list_default.json", "w" )
+f.write( json.dumps( foot_lst, indent=2 ) )
+f.close()
+
+

3615e5a0621d3c2ef5aaa063e5a46cb658da48d2
gabebuckmaster <buckmaster.gabe@gmail.com>
2016-06-21 21:51:28 -0700

    Fixed register links
    Fixed the broken register links on the about, blog, and feedback pages,
    and updated the verbiage on the button to match the current home page.

--- cgi/about.py
+++ cgi/about.py
@@ -16,8 +16,8 @@ login_signup="""
       <button class='btn btn-success' type='submit'>Log in</button>
     </form>
 
-    <form action='/signup' style='display:inline;' >
-      <button class='btn btn-warning' type='submit'>Sign up!</button>
+    <form action='/register' style='display:inline;' >
+      <button class='btn btn-warning' type='submit'>Register!</button>
     </form>
 
   </li>
